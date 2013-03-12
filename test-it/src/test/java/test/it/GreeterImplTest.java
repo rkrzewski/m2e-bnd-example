7c5e9d619a987150bc2b2ced6e8b9516c0b9a491
Rafał Krzewski <rafal.krzewski@caltha.pl>
2013-03-13 00:35:12 +0100

    A low-tech solution for detecting if the test was launched under JDT JUnit runner or maven-surefire-plugin and provisioning workspace bundles from appropriate locations.

--- test-it/src/test/java/test/it/GreeterImplTest.java
+++ test-it/src/test/java/test/it/GreeterImplTest.java
@@ -23,16 +23,35 @@ import test.Greeter;
 @RunWith(PaxExam.class)
 public class GreeterImplTest {
 
+	private boolean isEclipseRun;
+
+	private boolean isEclipseRun() {
+		StackTraceElement[] trace = new RuntimeException().getStackTrace();
+		for (StackTraceElement frame : trace) {
+			if (frame.getClassName().contains("org.eclipse.jdt")) {
+				System.out.println("JDT test run - using Bndtools output");
+				return true;
+			}
+		}
+		System.out.println("Surefire test run - using maven output");
+		return false;
+	}
+
+	private Option workspaceBundle(String module) {
+		return bundle(String.format("file:../%1$s/%2$s/%1$s.jar", module,
+				isEclipseRun ? "target/bnd" : "target"));
+	}
+
 	@Configuration
 	public Option[] paxExamConfig() throws IOException {
+		isEclipseRun = isEclipseRun();
 		return options(
 				junitBundles(),
 				mavenBundle(maven("org.apache.felix", "org.apache.felix.scr",
 						"1.6.2")),
 				mavenBundle(maven("org.apache.felix",
 						"org.apache.felix.metatype", "1.0.6")),
-				bundle("file:../test-api/target/bnd/test-api.jar"),
-				bundle("file:../test-impl/target/bnd/test-impl.jar"));
+				workspaceBundle("test-api"), workspaceBundle("test-impl"));
 	}
 
 	@Inject
